cd39af237f082cac40026e5cc38071c5f807dd4d
Kyle Mayer <x@kylemayer.dev>
2021-10-10 10:26:16 -0700

    tailwind css, headless ui, heroicons, craco init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
